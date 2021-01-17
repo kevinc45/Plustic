fbab736eca5019b55a0026a28edc67ca64f13176
KC <kevin45christian@gmail.com>
2021-01-17 20:36:18 +0700

    Applying template on About Us page

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
-<html>
+@extends('test')
+{{-- <!DOCTYPE html>
+<html>--}}
 <head>
     <title>About Us - PLUSTIC</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../assets/img/brand/favicon.png">
 </head>
+@section('Header')
+<h1>About Us</h1>
+@endsection
+@section('Content')
 <style>
     .center {
   display: block;
@@ -27,10 +32,9 @@
         font-family: system-ui;
     }
 </style>
-<body>
-<a href="/">< Back</a>
+{{-- <body> --}}
     <div>
-    <a href="/"><img src="{{ asset('argon') }}/img/brand/LogoPlusticPNG.png" class="center" alt="Logo PLUSTIC.ID" width="50%"></a>
+    {{-- <a href="/"><img src="{{ asset('argon') }}/img/brand/LogoPlusticPNG.png" class="center" alt="Logo PLUSTIC.ID" width="50%"></a> --}}
     <h1><center>About Us</h1>
     <h3><center>An Environment-Loving Group of People.</h3>
     
@@ -50,5 +54,6 @@
         <li>Mengajak masyarakat Indonesia untuk dapat tertarik dalam mengurangi limbah plastik, dan</li>
         <li>Memperkenalkan organisasi-organisasi pecinta lingkungan kepada masyarakat luas.</li>
     </ol>
-</body>
-</html>
+@endsection
+{{-- </body>
+</html> --}}
